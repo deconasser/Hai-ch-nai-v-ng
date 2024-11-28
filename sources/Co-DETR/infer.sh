844f24b17591a24975b4b95c8960fa5870e4d6cc
Nguyễn Bá Thịnh <124879074+deconasser@users.noreply.github.com>
2024-11-28 15:07:24 +0700

    chore: upload chunking service

--- sources/Co-DETR/infer.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH=${PYTHONPATH}:/base
-python3 tools/infer.py
--- sources/Co-DETR/train.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH=${PYTHONPATH}:/base
-python3 tools/train.py /base/nextgen-cfg/co_dino_5scale_swin_large_16e_o365tococo.py --work-dir nextgen
